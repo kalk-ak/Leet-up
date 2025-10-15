d805c38cc635cb084a17f0cf0398ee6f3c759fef
Kaleb Aklilu <kalebaklilu3@gmail.com>
2025-10-15 10:50:51 -0400

    .

--- 942.di-string-match.cpp
+++ 942.di-string-match.cpp
@@ -1,8 +1,32 @@
 // @leet start
-class Solution {
-public:
-    vector<int> diStringMatch(string s) {
-        
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution
+{
+  public:
+    vector<int> diStringMatch(string s)
+    {
+        int i = 0;
+        int n = s.size();
+
+        vector<int> perm;
+        for (auto c : s)
+        {
+            if (c == 'I')
+            {
+                perm.push_back(i++);
+            }
+            else
+            {
+                perm.push_back(n--);
+            }
+        }
+        perm.push_back(n);
+        return perm;
     }
 };
 // @leet end
