677a5dfc311c8c35db889a19fc1aad4373c9448b
Kaleb Aklilu <kalebaklilu3@gmail.com>
2026-01-17 14:23:16 -0500

    .

--- 2529.maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529.maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,8 +1,32 @@
 // @leet start
-class Solution {
-public:
-    int maximumCount(vector<int>& nums) {
-        
+class Solution
+{
+  public:
+    int maximumCount(vector<int> &nums)
+    {
+        // LOGIC: Get the size of the array. Count the Negeative numbers, pass the zero and get the
+        // positive with simple subtraction
+
+        // index and length
+        int i = 0, n = nums.size();
+
+        int negagtive_count = 0;
+        while (i < n)
+        {
+            int curr = nums[i];
+
+            if (curr < 0)
+                negagtive_count += 1;
+
+            else if (curr > 0)
+                break;
+
+            ++i;
+        }
+
+        int positive_count = n - i;
+
+        return (negagtive_count > positive_count) ? negagtive_count : positive_count;
     }
 };
 // @leet end
