4c974ca3f7873f433af7135565555788c1335d1a
Kaleb Aklilu <kalebaklilu3@gmail.com>
2026-01-17 14:52:30 -0500

    .

--- 27.remove-element.cpp
+++ 27.remove-element.cpp
@@ -1,8 +1,33 @@
 // @leet start
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+  public:
+    int removeElement(std::vector<int> &nums, int val)
+    {
+        // LOGIC: Removing from an array is o(n) so instead I would swap with the last element and
+        // then pop later
+
+        int i = 0, n = nums.size();
+        while (i < n)
+        {
+            if (nums[i] == val)
+            {
+                // swap with the last element
+                nums[i] = nums[n - 1];
+
+                // decrease n
+                --n;
+            }
+            else
+                ++i;
+        }
+
+        // pop from the back
+        nums.resize(i);
+        return i;
     }
 };
 // @leet end
